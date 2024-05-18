5052bfb54b34ab20b2246243fd814a4811e4d7ab
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-05-18 07:16:51 +0300

    moved task class from worker.py

--- worker/worker.py
+++ worker/worker.py
@@ -5,37 +5,11 @@ from tqueue import TaskQueue, QueueConnection
 from scraper.scraper import Scraper
 from analyzer.analyzer import Analyzer
 import logging
+from task import Task
 
 logger = logging.getLogger()
 
 TASK_QUEUE_NAME = "tasks"
-TASK_STATUS_MAP = json.loads(open("task_status.json").read())
-
-
-class Task:
-    def __init__(self, status=None, data=None):
-        self.status = status or TASK_STATUS_MAP["created"]
-        self.data = data or {
-            "user": None,
-            "tweets": None,
-            "analysis": None,
-            "message": None,
-            "result": None,
-        }
-    def set_status(self, status_str):
-        self.status = TASK_STATUS_MAP.get(status_str, TASK_STATUS_MAP["created"])
-
-    def serialize(self):
-        return json.dumps({
-            "status": self.status,
-            "data": self.data,
-        })
-
-    def deserialize(self, data):
-        data = json.loads(data)
-        self.status = data["status"]
-        self.data = data["data"]
-
 
 class Worker:
     received_task = {}
