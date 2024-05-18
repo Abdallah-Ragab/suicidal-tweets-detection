ce2b471d92025dbe4e1e3cc1ca4b7405c03906ab
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-05-18 07:07:01 +0300

    renamed queue.py tp tqueue.py,
    added message, result to the data dict of task obj,
    
    added conditions in scrape func to deal with failed scrape attempts.
    
    created analyze func to run tweets throught the ai model,
    
    created result func to add determinate results to the task

--- worker/worker.py
+++ worker/worker.py
@@ -1,7 +1,7 @@
 import json
 import os
 import pika
-from queue import TaskQueue, QueueConnection
+from tqueue import TaskQueue, QueueConnection
 from scraper.scraper import Scraper
 from analyzer.analyzer import Analyzer
 import logging
@@ -19,6 +19,8 @@ class Task:
             "user": None,
             "tweets": None,
             "analysis": None,
+            "message": None,
+            "result": None,
         }
     def set_status(self, status_str):
         self.status = TASK_STATUS_MAP.get(status_str, TASK_STATUS_MAP["created"])
@@ -42,7 +44,7 @@ class Worker:
         logger.info("Initializing worker...")
         self.connection = QueueConnection()
         self.queue = TaskQueue(self.connection, TASK_QUEUE_NAME, self.callback)
-        self.scraper = Scraper(20)
+        self.scraper = Scraper(5)
         self.analyzer = Analyzer()
         self.ready()
 
@@ -62,18 +64,56 @@ class Worker:
     def do(self):
         username = self.received_task["body"].decode()
         print(f"Received task: {username}")
-        print(self.scrape(username))
+        print(f"Scraped {username}. result: {self.scrape(username)}")
+        print(f"Analyzed {username}. result: {self.analyze()}")
+        print(f"Result: {self.result()}")
+        print(f"Task status: {self.task.serialize()}")
 
     def scrape(self, username):
         logger.info(f"Scraping {username}...")
+        self.task.set_status("running_1")
         result = self.scraper.tweets(username)
         if result:
+            tweets = [ tweet['text'] for tweet in result["tweets"]]
+            if len(tweets) == 0:
+                self.task.set_status("failed")
+                self.task.data["message"] = "No tweets found"
+                return False
             self.task.data["user"] = username
-            self.task.data["tweets"] = result
-            self.task.set_status("scraped")
+            self.task.data["tweets"] = tweets
+            return tweets
+        return False
+
+    def analyze(self):
+        if not self.task.data["tweets"]:
+            return False
+        logger.info(f"Analyzing tweets...")
+        self.task.set_status("running_2")
+        tweets = self.task.data["tweets"]
+        try:
+            result = self.analyzer.analyze(tweets)
+            self.task.data["analysis"] = result[0]
             return result
-        return
+        except Exception as e:
+            self.task.set_status("failed")
+            self.task.data["message"] = "Failed to analyze tweets"
+            return False
 
-
-
+    def result(self):
+        if not self.task.data["analysis"]:
+            return False
+        potential_suicidal_tweets = [
+            tweet_id for tweet_id, tweet in enumerate(self.task.data["tweets"]) if self.task.data["analysis"][tweet_id] == 1
+        ]
+        pst = len(potential_suicidal_tweets)
+        total = len(self.task.data["tweets"])
+        suicidal = pst > 0
+        message = f"We have found {pst} potential suicidal tweets. We recommend getting this person help." if suicidal else "No potential suicidal tweets found."
+        self.task.data["result"] = {
+            "suicidal": suicidal,
+            "total_tweets": total,
+            "ps_tweets": potential_suicidal_tweets,
+            "ps_count": pst,
+            "message": message,
+        }
 
