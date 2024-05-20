aec611db00959a935c7e2af41c5a0178a44488fa
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-05-20 05:20:38 +0300

    Refactor server.py to include CORS middleware for allowing cross-origin requests, refactored /task endpoint to use new model functions, added /check endpoint

--- api/server.py
+++ api/server.py
@@ -2,15 +2,46 @@
 from api.queue import TaskQueue, QueueConnection
 import fastapi
 import logging
-from task import Task
+from task import Task, update_task
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from fastapi.middleware.cors import CORSMiddleware
+
+
+engine = create_engine('sqlite:///tasks.db')
+Session = sessionmaker(bind=engine)
 
 app = fastapi.FastAPI()
 queue = TaskQueue(QueueConnection(), "tasks")
 logger = logging.getLogger()
 
+
+origins = [
+    "*",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/task")
 def get_task(username : str):
-    task = Task()
-    task.data["user"] = username
-    json_task = task.serialize()
-    queue.send(json_task)
+    task = Task(username=username)
+    task_id = task.id
+    task.save()
+    queue.send(task_id)
+    update_task(task_id, status='waiting')
+    return {"task_id": task_id}
+
+
+@app.get("/check")
+def check_task(task_id: str):
+    with Session() as session:
+        task = session.query(Task).filter_by(id=task_id).first()
+        return task.as_dict()
